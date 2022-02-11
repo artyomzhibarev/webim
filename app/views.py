115c8cf8a04d31955a83d1137b2ba323bea62e85
artyomzhibarev <artyom.zhibarev@yandex.ru>
2022-02-12 02:22:06 +0300

    some hot fix

--- app/views.py
+++ app/views.py
@@ -12,9 +12,14 @@ class HomePageView(TemplateView):
 @login_required(login_url='')
 def logic(request):
     if request.method == 'GET':
-        rand_num = int(redis_instance.get('rand_num'))
-        return render(request, 'app/logic.html',
-                      context={'random_number': rand_num})
+        try:
+            rand_num = int(redis_instance.get('rand_num'))
+        except TypeError:
+            print(f"int() argument must be a string, a bytes-like object or a number, not {rand_num.__class__.__name__}")
+            rand_num = 0
+        finally:
+            return render(request, 'app/logic.html',
+                          context={'random_number': rand_num})
     return render(request, 'app/logic.html')
 
 # def system(request):
